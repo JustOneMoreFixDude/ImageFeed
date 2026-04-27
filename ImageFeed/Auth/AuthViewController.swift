e21ba6b7b1d41747ac703ca135d432607516f1ea
JustOneMoreFixDude <dmitrion@inbox.ru>
2026-04-27 14:05:57 +0300

    Sprint 10: finish WebView auth flow, delegate, navigation

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -1,5 +1,49 @@
 import UIKit
 
 final class AuthViewController: UIViewController {
+    private let showWebViewSegueIdentifier = "ShowWebView"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureBackButton()
+    }
+    
+    // prepare = хук перед переходом между экранами
+    // вызывается автоматические перед переходом на другой экран, что бы передать другому экрану данные
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showWebViewSegueIdentifier {
+            guard
+                let webViewViewController = segue.destination as? WebViewViewController
+            else {
+                assertionFailure("Failed to prepare for \(showWebViewSegueIdentifier)")
+                return
+            }
+            webViewViewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
+    
+    
+    //кастомная кнопка назад в Navigation Bar
+    private func configureBackButton() {
+        navigationController?.navigationBar.backIndicatorImage = UIImage(named: "nav_back_button")
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "nav_back_button")
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem?.tintColor = UIColor(named: "ypBlack")
+    }
+    
+}
+
+
+extension AuthViewController: WebViewViewControllerDelegate {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        //TODO: process code
+    }
+    
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        vc.dismiss(animated: true)
+    }
+    
     
 }
